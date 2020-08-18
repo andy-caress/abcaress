03639f30d2705385e9e0294186b8b4ee53c51e82
andy-caress <54376326+andy-caress@users.noreply.github.com>
2020-08-18 17:06:38 -0400

    all work on website, new resume

---
+++ work/coc/coc.js
@@ -0,0 +1,41 @@
+function menuShow() {
+  var element = document.getElementById("box");
+  element.classList.toggle("style");
+}
+
+/**
+ * Example of starting a plugin with options.
+ * I am just passing some of the options in the following example.
+ * you can also start the plugin using $('.marquee').marquee(); with defaults
+*/
+$('.marquee').marquee({
+	//duration in milliseconds of the marquee
+	duration: 7000,
+	//gap in pixels between the tickers
+	gap: 5,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true
+});
+
+$('.marquee-btn').marquee({
+	duration: 2500,
+	gap: 10,
+	delayBeforeStart: 0,
+	direction: 'left',
+	duplicated: true,
+  pauseOnHover: true
+});
+
+$('.marquee-btn2').marquee({
+	duration: 2500,
+	gap: 10,
+	delayBeforeStart: 0,
+	direction: 'left',
+	duplicated: true,
+  pauseOnHover: true
+});
